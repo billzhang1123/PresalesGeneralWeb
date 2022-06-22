157ec0b26865134bf16f2481e348e710cf8ea36c
billzhang1123 <billzhang1123@gmail.com>
2022-06-22 17:29:06 -0600

    Add files via upload

---
+++ scripts/config_xml.js
@@ -0,0 +1,54 @@
+var TSC = TSC || {};
+
+TSC.embedded_config_xml = '<x:xmpmeta tsc:version="2.0.1" xmlns:x="adobe:ns:meta/" xmlns:tsc="http://www.techsmith.com/xmp/tsc/">\
+   <rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:xmp="http://ns.adobe.com/xap/1.0/" xmlns:xmpDM="http://ns.adobe.com/xmp/1.0/DynamicMedia/" xmlns:xmpG="http://ns.adobe.com/xap/1.0/g/" xmlns:xmpMM="http://ns.adobe.com/xap/1.0/mm/" xmlns:tscDM="http://www.techsmith.com/xmp/tscDM/" xmlns:tscIQ="http://www.techsmith.com/xmp/tscIQ/" xmlns:tscHS="http://www.techsmith.com/xmp/tscHS/" xmlns:stDim="http://ns.adobe.com/xap/1.0/sType/Dimensions#" xmlns:stFnt="http://ns.adobe.com/xap/1.0/sType/Font#" xmlns:exif="http://ns.adobe.com/exif/1.0" xmlns:dc="http://purl.org/dc/elements/1.1/">\
+      <rdf:Description dc:date="2022-06-22 01:14:13 PM" dc:source="Camtasia,21.0.17,enu" dc:title="Landing Page" tscDM:firstFrame="Landing_Page_First_Frame.png" tscDM:originId="04028459-86CB-46C0-9CDD-D4DB0B45F780" tscDM:project="Landing Page">\
+         <xmpDM:duration xmpDM:scale="1/1000" xmpDM:value="796766"/>\
+         <xmpDM:videoFrameSize stDim:unit="pixel" stDim:h="652" stDim:w="1280"/>\
+         <tsc:langName>\
+            <rdf:Bag>\
+               <rdf:li xml:lang="en-US">English</rdf:li></rdf:Bag>\
+         </tsc:langName>\
+         <xmpDM:Tracks>\
+            <rdf:Bag>\
+               <rdf:li>\
+                  <rdf:Description xmpDM:trackType="Hotspot" xmpDM:frameRate="f1000" xmpDM:trackName="Hotspots">\
+                     <xmpDM:markers>\
+                        <rdf:Seq>\
+                           <rdf:li><rdf:Description xmpDM:label="1" xmpDM:startTime="2570" xmpDM:duration="6300" tscDM:boundingPoly="513,223;688,223;688,257;513,257;" tscDM:rotate="0.000000" tscHS:pause="1" tscHS:jumpTime="8867"/></rdf:li><rdf:li><rdf:Description xmpDM:label="2" xmpDM:startTime="2570" xmpDM:duration="6300" tscDM:boundingPoly="772,354;940,354;940,394;772,394;" tscDM:rotate="0.000000" tscHS:pause="1" tscHS:jumpTime="8867"/></rdf:li><rdf:li><rdf:Description xmpDM:label="3" xmpDM:startTime="2600" xmpDM:duration="6300" tscDM:boundingPoly="340,403;516,403;516,437;340,437;" tscDM:rotate="0.000000" tscHS:pause="1" tscHS:jumpTime="8867"/></rdf:li><rdf:li><rdf:Description xmpDM:label="4" xmpDM:startTime="2600" xmpDM:duration="6300" tscDM:boundingPoly="1124,602;1272,602;1272,637;1124,637;" tscDM:rotate="0.000000" tscHS:pause="1" tscHS:jumpTime="0"/></rdf:li></rdf:Seq>\
+                     </xmpDM:markers>\
+                  </rdf:Description>\
+               </rdf:li>\
+               <rdf:li>\
+                  <rdf:Description xmpDM:trackType="TableOfContents" xmpDM:frameRate="f1000" xmpDM:trackName="Table of Contents">\
+                     <xmpDM:markers>\
+                        <rdf:Seq>\
+                           <rdf:li><rdf:Description xmpDM:name="Start" xmpDM:startTime="0" tscDM:image="Landing_Page_Thumbnails.png" tscDM:imageindex="0" tscDM:imageoffset="0" tscDM:imagerect="0, 0, 75, 38"/></rdf:li><rdf:li><rdf:Description xmpDM:name="BlackHill" xmpDM:startTime="8867" tscDM:image="Landing_Page_Thumbnails.png" tscDM:imageindex="1" tscDM:imageoffset="0" tscDM:imagerect="75, 0, 75, 38"/></rdf:li></rdf:Seq>\
+                     </xmpDM:markers>\
+                  </rdf:Description>\
+               </rdf:li>\
+            </rdf:Bag>\
+         </xmpDM:Tracks>\
+         <tscDM:controller>\
+            <rdf:Description xmpDM:name="tscplayer">\
+               <tscDM:parameters>\
+                  <rdf:Bag>\
+                     <rdf:li xmpDM:name="autohide" xmpDM:value="true"/><rdf:li xmpDM:name="autoplay" xmpDM:value="false"/><rdf:li xmpDM:name="loop" xmpDM:value="false"/><rdf:li xmpDM:name="searchable" xmpDM:value="true"/><rdf:li xmpDM:name="captionsenabled" xmpDM:value="false"/><rdf:li xmpDM:name="sidebarenabled" xmpDM:value="false"/><rdf:li xmpDM:name="unicodeenabled" xmpDM:value="false"/><rdf:li xmpDM:name="backgroundcolor" xmpDM:value="000000"/><rdf:li xmpDM:name="sidebarlocation" xmpDM:value="left"/><rdf:li xmpDM:name="endaction" xmpDM:value="stop"/><rdf:li xmpDM:name="endactionparam" xmpDM:value="true"/><rdf:li xmpDM:name="locale" xmpDM:value="en-US"/></rdf:Bag>\
+               </tscDM:parameters>\
+               <tscDM:controllerText>\
+                  <rdf:Bag>\
+                  </rdf:Bag>\
+               </tscDM:controllerText>\
+            </rdf:Description>\
+         </tscDM:controller>\
+         <tscDM:contentList>\
+            <rdf:Description>\
+               <tscDM:files>\
+                  <rdf:Seq>\
+                     <rdf:li xmpDM:name="0" xmpDM:value="Landing Page.mp4"/><rdf:li xmpDM:name="1" xmpDM:value="Landing_Page_First_Frame.png"/><rdf:li xmpDM:name="2" xmpDM:value="Landing_Page_Thumbnails.png"/></rdf:Seq>\
+               </tscDM:files>\
+            </rdf:Description>\
+         </tscDM:contentList>\
+      </rdf:Description>\
+   </rdf:RDF>\
+</x:xmpmeta>';

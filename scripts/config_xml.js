75bad980295245e2f14720d320862e8a8024abcb
billzhang1123 <billzhang1123@gmail.com>
2022-06-23 18:28:32 -0600

    Add files via upload

--- scripts/config_xml.js
+++ scripts/config_xml.js
@@ -2,8 +2,8 @@ var TSC = TSC || {};
 
 TSC.embedded_config_xml = '<x:xmpmeta tsc:version="2.0.1" xmlns:x="adobe:ns:meta/" xmlns:tsc="http://www.techsmith.com/xmp/tsc/">\
    <rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:xmp="http://ns.adobe.com/xap/1.0/" xmlns:xmpDM="http://ns.adobe.com/xmp/1.0/DynamicMedia/" xmlns:xmpG="http://ns.adobe.com/xap/1.0/g/" xmlns:xmpMM="http://ns.adobe.com/xap/1.0/mm/" xmlns:tscDM="http://www.techsmith.com/xmp/tscDM/" xmlns:tscIQ="http://www.techsmith.com/xmp/tscIQ/" xmlns:tscHS="http://www.techsmith.com/xmp/tscHS/" xmlns:stDim="http://ns.adobe.com/xap/1.0/sType/Dimensions#" xmlns:stFnt="http://ns.adobe.com/xap/1.0/sType/Font#" xmlns:exif="http://ns.adobe.com/exif/1.0" xmlns:dc="http://purl.org/dc/elements/1.1/">\
-      <rdf:Description dc:date="2022-06-23 03:35:29 PM" dc:source="Camtasia,21.0.17,enu" dc:title="landingpage" tscDM:firstFrame="landingpage_First_Frame.png" tscDM:originId="04028459-86CB-46C0-9CDD-D4DB0B45F780" tscDM:project="landingpage">\
-         <xmpDM:duration xmpDM:scale="1/1000" xmpDM:value="8300"/>\
+      <rdf:Description dc:date="2022-06-23 06:25:01 PM" dc:source="Camtasia,21.0.17,enu" dc:title="landingpage-full-screen" tscDM:firstFrame="landingpage-full-screen_First_Frame.png" tscDM:originId="04028459-86CB-46C0-9CDD-D4DB0B45F780" tscDM:project="landingpage-full-screen">\
+         <xmpDM:duration xmpDM:scale="1/1000" xmpDM:value="19733"/>\
          <xmpDM:videoFrameSize stDim:unit="pixel" stDim:h="978" stDim:w="1920"/>\
          <tsc:langName>\
             <rdf:Bag>\
@@ -15,7 +15,7 @@ TSC.embedded_config_xml = '<x:xmpmeta tsc:version="2.0.1" xmlns:x="adobe:ns:meta
                   <rdf:Description xmpDM:trackType="Hotspot" xmpDM:frameRate="f1000" xmpDM:trackName="Hotspots">\
                      <xmpDM:markers>\
                         <rdf:Seq>\
-                           <rdf:li><rdf:Description xmpDM:label="1" xmpDM:startTime="2570" xmpDM:duration="4730" tscDM:boundingPoly="768,333;1031,333;1031,384;768,384;" tscDM:rotate="0.000000" tscHS:pause="0" xmpDM:location="https://esppresalesfilestore.blob.core.windows.net/attachment/BlackHills_R4.mp4" tscHS:newWindow="1"/></rdf:li><rdf:li><rdf:Description xmpDM:label="2" xmpDM:startTime="2570" xmpDM:duration="4730" tscDM:boundingPoly="1153,536;1432,536;1432,587;1153,587;" tscDM:rotate="0.000000" tscHS:pause="0" xmpDM:location="https://esppresalesfilestore.blob.core.windows.net/attachment/BlackHills_R4.mp4" tscHS:newWindow="1"/></rdf:li><rdf:li><rdf:Description xmpDM:label="3" xmpDM:startTime="2570" xmpDM:duration="4730" tscDM:boundingPoly="508,605;773,605;773,655;508,655;" tscDM:rotate="0.000000" tscHS:pause="0" xmpDM:location="https://esppresalesfilestore.blob.core.windows.net/attachment/BlackHills_R4.mp4" tscHS:newWindow="1"/></rdf:li></rdf:Seq>\
+                           <rdf:li><rdf:Description xmpDM:label="1" xmpDM:startTime="7430" xmpDM:duration="12300" tscDM:boundingPoly="420,369;683,369;683,419;420,419;" tscDM:rotate="0.000000" tscHS:pause="0" tscHS:jumpTime="8867"/></rdf:li><rdf:li><rdf:Description xmpDM:label="2" xmpDM:startTime="7430" xmpDM:duration="12300" tscDM:boundingPoly="1461,783;1740,783;1740,834;1461,834;" tscDM:rotate="0.000000" tscHS:pause="0" tscHS:jumpTime="8867"/></rdf:li><rdf:li><rdf:Description xmpDM:label="3" xmpDM:startTime="7430" xmpDM:duration="12300" tscDM:boundingPoly="329,526;593,521;594,571;330,577;" tscDM:rotate="0.000000" tscHS:pause="0" tscHS:jumpTime="8867"/></rdf:li><rdf:li><rdf:Description xmpDM:label="4" xmpDM:startTime="7430" xmpDM:duration="12300" tscDM:boundingPoly="1044,399;1309,399;1309,450;1044,450;" tscDM:rotate="0.000000" tscHS:pause="0" tscHS:jumpTime="8867"/></rdf:li></rdf:Seq>\
                      </xmpDM:markers>\
                   </rdf:Description>\
                </rdf:li>\
@@ -23,7 +23,7 @@ TSC.embedded_config_xml = '<x:xmpmeta tsc:version="2.0.1" xmlns:x="adobe:ns:meta
                   <rdf:Description xmpDM:trackType="TableOfContents" xmpDM:frameRate="f1000" xmpDM:trackName="Table of Contents">\
                      <xmpDM:markers>\
                         <rdf:Seq>\
-                           <rdf:li><rdf:Description xmpDM:name="Start" xmpDM:startTime="0" tscDM:image="landingpage_Thumbnails.png" tscDM:imageindex="0" tscDM:imageoffset="0" tscDM:imagerect="0, 0, 75, 38"/></rdf:li></rdf:Seq>\
+                           <rdf:li><rdf:Description xmpDM:name="Start" xmpDM:startTime="0" tscDM:image="landingpage-full-screen_Thumbnails.png" tscDM:imageindex="0" tscDM:imageoffset="0" tscDM:imagerect="0, 0, 75, 38"/></rdf:li></rdf:Seq>\
                      </xmpDM:markers>\
                   </rdf:Description>\
                </rdf:li>\
@@ -45,7 +45,7 @@ TSC.embedded_config_xml = '<x:xmpmeta tsc:version="2.0.1" xmlns:x="adobe:ns:meta
             <rdf:Description>\
                <tscDM:files>\
                   <rdf:Seq>\
-                     <rdf:li xmpDM:name="0" xmpDM:value="landingpage.mp4"/><rdf:li xmpDM:name="1" xmpDM:value="landingpage_First_Frame.png"/><rdf:li xmpDM:name="2" xmpDM:value="landingpage_Thumbnails.png"/></rdf:Seq>\
+                     <rdf:li xmpDM:name="0" xmpDM:value="landingpage-full-screen.mp4"/><rdf:li xmpDM:name="1" xmpDM:value="landingpage-full-screen_First_Frame.png"/><rdf:li xmpDM:name="2" xmpDM:value="landingpage-full-screen_Thumbnails.png"/></rdf:Seq>\
                </tscDM:files>\
             </rdf:Description>\
          </tscDM:contentList>\
